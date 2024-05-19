b374b80c260d0aee5ae326f2bb5492eed94c40bc
Anastasiya <tiikhomirrovaa@mail.ru>
2024-05-19 18:47:15 +0300

    Доработка с общим исключением

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -5,37 +5,66 @@ namespace Calculator
     class Program
     {
         static void Main(string[] args)
-        {           
-            Console.WriteLine("Введите первое число:");
-            double num1 = Convert.ToDouble(Console.ReadLine());
+        {
+            Console.WriteLine("Простой консольный калькулятор");
 
-            Console.WriteLine("Введите оператор (+, -, *, /):");
-            char operation = Console.ReadLine()[0];
-
-            Console.WriteLine("Введите второе число:");
-            double num2 = Convert.ToDouble(Console.ReadLine());
-
-            double result = 0;
-            switch (operation)
+            try
             {
-                case '+':
-                    result = num1 + num2;
-                    break;
-                case '-':
-                    result = num1 - num2;
-                    break;
-                case '*':
-                    result = num1 * num2;
-                    break;
-                case '/':
-                    result = num1 / num2;
-                    break;
-                default:
-                    Console.WriteLine("Неправильный оператор");
-                    break;
-            }
+                // Ввод первого числа
+                Console.WriteLine("Введите первое число:");
+                double num1 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine($"Результат: {result}");
+                // Ввод оператора
+                Console.WriteLine("Введите оператор (+, -, *, /):");
+                char operation = Console.ReadLine()[0];
+
+                // Ввод второго числа
+                Console.WriteLine("Введите второе число:");
+                double num2 = Convert.ToDouble(Console.ReadLine());
+
+                // Вычисление результата в зависимости от оператора
+                double result = 0;
+                switch (operation)
+                {
+                    case '+':
+                        result = num1 + num2;
+                        break;
+                    case '-':
+                        result = num1 - num2;
+                        break;
+                    case '*':
+                        result = num1 * num2;
+                        break;
+                    case '/':
+                        if (num2 == 0)
+                        {
+                            throw new DivideByZeroException("Деление на ноль невозможно.");
+                        }
+                        result = num1 / num2;
+                        break;
+                    default:
+                        throw new ArgumentException("Некорректное действие. Введите один из следующих операторов: +, -, *, /");
+                }
+
+                // Вывод результата
+                Console.WriteLine($"Результат: {result}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка: Введите корректное число.");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Неизвестная ошибка: {ex.Message}");
+            }
         }
     }
 }
