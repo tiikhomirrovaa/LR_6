7265fd991a924de7d72cbb950fccc2ef2e1dbc5e
Anastasiya <tiikhomirrovaa@mail.ru>
2024-05-19 17:48:47 +0300

    Добавление калькулятора

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+            Console.WriteLine("Введите первое число:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите оператор (+, -, *, /):");
+            char operation = Console.ReadLine()[0];
+
+            Console.WriteLine("Введите второе число:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double result = 0;
+            switch (operation)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    break;
+                default:
+                    Console.WriteLine("Неправильный оператор");
+                    break;
+            }
+
+            Console.WriteLine($"Результат: {result}");
+        }
+    }
+}
